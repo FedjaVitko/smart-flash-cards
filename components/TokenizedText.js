16c9419635778d15051f3f1eec540eb9f14a6855
TheoVitkovskiy <fvitkovski@mail.de>
2018-08-19 11:59:54 +0200

    Add buttons

--- components/TokenizedText.js
+++ components/TokenizedText.js
@@ -1,45 +1,19 @@
 import React, { Component } from 'react';
 import { Text, StyleSheet } from 'react-native';
 
-class TokenizedText extends Component {
-
-    state = {
-        tokenColors: {}
-    }
-
-    componentWillReceiveProps(nextProps) {
-        const tokenColors = nextProps.tokens.reduce((tokenColors, token) => {
-            return tokenColors[token] = 'white';
-        }, {});
-
-        this.setState({ tokenColors });
-    }
-
-    render() {
-        return this.props.tokens.map((token, id) => {
+const TokenizedText = ({ tokens, onPress, tokenColors }) => (
+        tokens.map((token, id) => {
             return (
                 <Text
                     key={`${token}:${id}`} 
-                    onPress={() => { 
-                        console.log(token);
-                        // this.setState(prevState => ({
-                        //     tokenColors : {
-                        //         ...prevState.tokenColors,
-                        //         token: 'green'
-                        //     }
-                        // }))
-                        let newState = Object.assign({}, this.state);
-                        newState.tokenColors[token] = 'green';
-                        console.log(this.state.tokenColors);
-                    }}
-                    style={{ backgroundColor: this.state.tokenColors[token] }}
+                    onPress={() => onPress(token)}
+                    style={{ backgroundColor: tokenColors[token] }}
                 >
                     {`${token} `}
                 </Text>
             );
-        });
-    }
-}
+        })
+    );
 
 export default TokenizedText;
 
--- screens/CreateCardScreen.js
+++ screens/CreateCardScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native'
-import { Container, Header, Content, Textarea, Form, Text, Card, CardItem, Body, Title } from 'native-base';
+import { Container, Header, Content, Textarea, Form, Text, Card, CardItem, Body, Title, Button } from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 import TokenizedText from '../components/TokenizedText';
@@ -10,8 +11,22 @@ class CreateCardScreen extends Component {
     state = {
         question: '',
         answer: '',
-        tokenizedAnswer: {}
+        tokenizedAnswer: {},
+        tokenColors: {},
+        selectedColor: 'green'
     };
+
+    componentWillReceiveProps(nextProps) {
+      let tokenColors = this.state.tokenColors;
+      if (!this.state.tokenColors) {
+          tokenColors = Object.keys(this.state.tokenizedAnswer).reduce((tokenColors, token) => {
+              tokenColors[token] = 'white';
+              return tokenColors;
+          }, {});
+      }
+
+      this.setState({ tokenColors });
+  }
 
     tokenizeAnswer = () => {
       const stringToTokenize = this.state.answer;
@@ -22,10 +37,17 @@ class CreateCardScreen extends Component {
         answer[token] = 0;
         return answer;
       }, {});
-
-      console.log(tokenizedAnswer);
     
       this.setState({ tokenizedAnswer });
+    }
+
+    onPress = (token) => { 
+      const { tokenColors, selectedColor } = this.state;
+
+      let newTokenColors = Object.assign({}, tokenColors);
+      newTokenColors[token] = selectedColor;
+
+      this.setState({ tokenColors: newTokenColors });          
     }
 
     render() {
@@ -58,10 +80,35 @@ class CreateCardScreen extends Component {
                     <Body>
                       <Text>
                         <TokenizedText
+                          onPress={this.onPress}
                           tokens={Object.keys(this.state.tokenizedAnswer)}
+                          tokenColors={this.state.tokenColors}
+                          selectedColor={this.state.selectedColor}
                         />
                       </Text>
                     </Body>
+                  </CardItem>
+                  <CardItem footer bordered>
+                    <Grid>
+                      <Col>
+                        <Row>
+                          <Button rounded onPress={() => this.setState({ selectedColor: 'white' })} style={{ width: '50%' }}>
+                            <Text style={styles.buttonText}>Not Important</Text>
+                          </Button>
+                          <Button rounded success onPress={() => this.setState({ selectedColor: 'green' })} style={{ width: '50%' }}>
+                            <Text style={styles.buttonText}>Less Important</Text>
+                          </Button>
+                        </Row>
+                        <Row>
+                          <Button rounded warning onPress={() => this.setState({ selectedColor: 'orange' })} style={{ width: '50%' }}>
+                            <Text style={styles.buttonText}>Important</Text>
+                          </Button>
+                          <Button rounded danger onPress={() => this.setState({ selectedColor: 'red' })} style={{ width: '50%' }}>
+                            <Text style={styles.buttonText}>Very Important</Text>
+                          </Button>
+                        </Row>
+                      </Col>
+                    </Grid>
                   </CardItem>
                 </Card>
               </Form>
@@ -77,5 +124,9 @@ const styles = StyleSheet.create({
   header: {
     height: 44 + getStatusBarHeight(),
     backgroundColor: 'orange',
+  },
+  buttonText: {
+    fontSize: 10,
+    textAlign: 'center'
   }
 })
